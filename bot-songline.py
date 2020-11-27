0554e9249f1573bfc7d20c51477b17a7ae552e26
PymPhekasut <74609915+PymPhekasut@users.noreply.github.com>
2020-11-27 22:00:26 +1100

    Update bot-songline.py

--- bot-songline.py
+++ bot-songline.py
@@ -4,6 +4,6 @@ token = 'FM9m4KGs9rsVRzpTEZCGwdbQn5WcBOGE6CDElqSV3fF'
 
 messenger = Sendline(token)
 
-#messenger.sendtext('Hi, how are you?')
-#messenger.sticker(8,1)
-messenger.sendimage('https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg')
+messenger.sendtext('Hi, how are you?') #send text
+messenger.sticker(8,1) #send sticker
+messenger.sendimage('https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg') #send picture

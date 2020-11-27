50485ea165a1162ddbc1a7e6e8f67a4343f2845b
PymPhekasut <74609915+PymPhekasut@users.noreply.github.com>
2020-11-27 21:59:50 +1100

    Add files via upload

---
+++ bot-songline.py
@@ -0,0 +1,9 @@
+from songline import Sendline
+
+token = 'FM9m4KGs9rsVRzpTEZCGwdbQn5WcBOGE6CDElqSV3fF'
+
+messenger = Sendline(token)
+
+#messenger.sendtext('Hi, how are you?')
+#messenger.sticker(8,1)
+messenger.sendimage('https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg')
